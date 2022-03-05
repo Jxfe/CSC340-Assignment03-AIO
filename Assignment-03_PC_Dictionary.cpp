66626b54f83b7fb92e282f5502118242fe2e2de2
Jeff <ma.jeff126@gmail.com>
2022-03-05 11:45:19 -0800

    setting up DIctionary

--- Assignment-03_PC_Dictionary.cpp
+++ Assignment-03_PC_Dictionary.cpp
@@ -1,9 +1,59 @@
 #include<iostream>
 #include<string>
+#include<fstream>
 #include<vector>
 #include<iomanip>
+#include<map>
+#include<list>
+#include<algorithm>
+
 using namespace std;
 
+class Dictionary {
+    private:
+        string word;
+        string partOfSpeech;
+        vector<string> definition;
+
+    public:
+        Dictionary(string w, string pos, vector<string> d) {
+            word = w;
+            partOfSpeech = pos;
+            definition = d;
+        }
+        string getWord() {
+            return word;
+        }
+        string getPartOfSpeech() {
+            return partOfSpeech;
+        }
+        vector<string> getDefinition() {
+            return definition;
+        }
+};
+
 int main() {
+    string fileLocation = "./Data.CS.SFSU.txt";
+    map<string, vector<string>> hmap;
+    bool fileOpen = false;
+    ifstream fin(fileLocation);
+
+    while(fileOpen == false) {
+        ifstream fin(fileLocation);
+        if(fin.is_open()) {
+            cout << "! Opening data file... " << fileLocation << endl;
+            cout << "! Loading data..." << endl;
+            break;
+        }
+        else {
+            cout << "<!>ERROR<!> ===> File could not be opened." << endl;
+            cout << "<!>ERROR<!> ===> Provided file path: " << fileLocation << endl;
+            cout << "<!>Enter the CORRECT data file path: ";
+            cin >> fileLocation;
+        }
+    }
+    cout << "! Closing data file... " << fileLocation << endl;
+    fin.close();
+
     return 0;
 }
